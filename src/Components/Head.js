aebcc4a08f22366b96ee45e1958419b9d1a2134d
Akshat <akshatastra@gmail.com>
2024-04-13 17:21:42 +0530

    removed theme mode

--- src/Components/Head.js
+++ src/Components/Head.js
@@ -1,4 +1,4 @@
-
+/* 
 import React, { useState } from 'react';
 
 const Head = () => {
@@ -29,4 +29,45 @@ const Head = () => {
   );
 };
 
-export default Head
+export default Head */
+
+
+
+
+
+
+
+
+
+
+import React from 'react';
+
+const Head = () => {
+  /* const [isdark, setisdark] = useState(true);
+
+  const handleClick = () => {
+    document.body.classList.toggle('light-theme');
+    setisdark(!isdark);
+  }; */
+
+  return (
+    <div>
+      <header className="header flex justify-between items-center px-4 py-3 bg-gray-800 text-white">
+        <div>
+          <h1 className="text-2xl">Where in the world?</h1>
+        </div>
+
+       {/*  <div className="dark-mode-btn">
+          <button
+            className={`bg-${isdark ? 'blue-500' : 'yellow-500'} hover:bg-${isdark ? 'blue-700' : 'yellow-700'} text-white font-bold py-2 px-4 rounded`}
+            onClick={handleClick}
+          >
+            {isdark ? 'Light Mode ☀️' : 'Dark Mode🌙'}
+          </button>
+        </div> */}
+      </header>
+    </div>
+  );
+};
+
+export default Head;
