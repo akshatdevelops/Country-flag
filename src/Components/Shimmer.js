281a44b0180317f01d625d24ee9eb59ea3118d09
Akshat <akshatastra@gmail.com>
2024-04-15 13:23:36 +0530

    added shimmer ui

---
+++ src/Components/Shimmer.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Shimmer = () => {
+    return (
+
+
+        <div className="card bg-white rounded-lg shadow-md overflow-hidden">
+
+            <img src="" alt="" className="w-full h-40 object-cover" />
+            <div className="p-4">
+                <strong className="text-lg font-bold">Loading....</strong>
+                <p className="text-sm text-black-600 font-serif"></p>
+                <p className="text-sm text-black-600 font-serif"></p>
+                <p className="text-sm text-black-600 font-serif"></p>
+
+            </div>
+
+
+        </div>
+
+    )
+}
+
+export default Shimmer
