98654dbaa51c436418667825158e8da931c7a4ab
Akshat <akshatastra@gmail.com>
2024-04-15 09:56:43 +0530

    removed learn more from card and applied navigation feature to whole card

--- src/Components/CountryList.js
+++ src/Components/CountryList.js
@@ -9,20 +9,18 @@ const CountryList = ({ countryList }) => {
         {countryList.map((country, i) => {
           const { name, population, region, capital, flags } = country;
           return (
+
             <div key={name.common} className="card bg-white rounded-lg shadow-md overflow-hidden">
+              <Link to={`/countries/${name.common}`}>
               <img src={flags.svg} alt={name.common} className="w-full h-40 object-cover" />
               <div className="p-4">
                 <h4 className="text-lg font-semibold">{name.common}</h4>
                 <p className="text-sm text-gray-600 font-serif">Population: {population}</p>
                 <p className="text-sm text-gray-600 font-serif">Region: {region}</p>
                 <p className="text-sm text-gray-600 font-serif">Capital: {capital}</p>
-                <Link
-                  to={`/countries/${name.common}`}
-                  className="block mt-2 text-blue-500 hover:underline text-sm"
-                >
-                  Learn more
-                </Link>
+                
               </div>
+              </Link>
 
             </div>
           );
